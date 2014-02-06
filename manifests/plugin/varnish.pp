018de4c319244b7d4f54d13f902f3f7e568f765b
Wyatt Walter <wyattwalter@gmail.com>
2014-02-06 13:23:01 -0600

    varnish module lint cleanups

--- manifests/plugin/varnish.pp
+++ manifests/plugin/varnish.pp
@@ -3,15 +3,15 @@ class collectd::plugin::varnish (
   $ensure           = present,
   $instances        = {
       'localhost' => {
-        'CollectCache' => 'true',
-        'CollectBackend' => 'true',
-        'CollectConnections' => 'true',
-        'CollectSHM' => 'true',
-        'CollectESI' => 'false',
-        'CollectFetch' => 'true',
-        'CollectHCB' => 'false',
-        'CollectTotals' => 'true',
-        'CollectWorkers' => 'true',
+        'CollectCache' => true,
+        'CollectBackend' => true,
+        'CollectConnections' => true,
+        'CollectSHM' => true,
+        'CollectESI' => false,
+        'CollectFetch' => true,
+        'CollectHCB' => false,
+        'CollectTotals' => true,
+        'CollectWorkers' => true,
     }
   }
 ) {
