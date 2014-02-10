18aae8dc3985ca3b50188057d57d10ecf37543f7
Wyatt Walter <wyattwalter@gmail.com>
2014-02-10 17:34:28 -0600

    allow multiple memcache hosts in template

--- manifests/plugin/memcached.pp
+++ manifests/plugin/memcached.pp
@@ -1,10 +1,12 @@
 # https://collectd.org/wiki/index.php/Plugin:memcached
 class collectd::plugin::memcached (
   $ensure = present,
-  $host   = '127.0.0.1',
+  $hosts  = ['127.0.0.1'],
   $port   = '11211',
 ) {
   include collectd::params
+
+  validate_array($hosts)
 
   $conf_dir = $collectd::params::plugin_conf_dir
 
